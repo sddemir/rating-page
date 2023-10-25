016a9f72a791706b48b25eca473a9741c40e70a0
Sümeyye Demir <sumeyyedemir1903@gmail.com>
2023-10-25 21:36:42 +0300

    needed form

--- rating.js
+++ rating.js
@@ -1,7 +1,12 @@
-const btns= document.querySelectorAll('.btn');
+const form= document.querySelector('.form');
+const btns= form.querySelectorAll('.btn');
+
 
 //btns is a nodelist so you can't add addEventListener to all of them without forEach
 btns.forEach((btn)=>{
  btn.addEventListener("click", ()=>{
     let score = btn.textContent;
     console.log(score);});});
+form.addEventListener("sumbit", (e)=>{
+    e.preventDefault();
+})
