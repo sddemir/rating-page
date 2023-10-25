2853fb869d4ca97dbe64343b2d48d52a66a41dd0
Sümeyye Demir <sumeyyedemir1903@gmail.com>
2023-10-25 17:27:24 +0300

    form

--- rating.js
+++ rating.js
@@ -1,5 +1,6 @@
 const btns= document.querySelectorAll('.btn');
 
+//btns is a nodelist so you can't add addEventListener to all of them without forEach
 btns.forEach((btn)=>{
  btn.addEventListener("click", ()=>{
     let score = btn.textContent;
